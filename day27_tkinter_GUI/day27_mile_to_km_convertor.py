148cc1f01d91bcb42852b5fde940e5ced312c295
Kevin Chunye <53375755+KevinChunye@users.noreply.github.com>
2022-07-09 02:08:21 -0500

    Add files via upload
    miles to km convertor GUI application

---
+++ day27_tkinter_GUI/day27_mile_to_km_convertor.py
@@ -0,0 +1,28 @@
+from tkinter import *
+
+def miles_to_km():
+    miles = float(miles_input.get())
+    km = round(miles * 1.609,2)
+    km_result_label.config(text=f"{km}")
+window = Tk()
+window.title("Miles to Km convertor")
+
+miles_input = Entry()
+miles_input.grid(column=1, row=0)
+
+miles_label = Label(text = "Miles")
+miles_label.grid(column=2 , row=0)
+
+is_equal_label = Label(text ="is equal to")
+is_equal_label.grid(column=0 , row=1)
+
+km_result_label = Label(text="0")
+km_result_label.grid(column=1 , row=1)
+
+km_label = Label(text="Km")
+km_label.grid(column=2 , row=1)
+
+calculate_button = Button(text="Calculate", command = miles_to_km)
+calculate_button.grid(column=1, row=2)
+
+window.mainloop()
