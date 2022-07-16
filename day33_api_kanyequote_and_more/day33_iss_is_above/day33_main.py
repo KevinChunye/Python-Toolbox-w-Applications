1b57db4305a7f41832d96f6fd0a5be76aa8551c6
Kevin Chunye <53375755+KevinChunye@users.noreply.github.com>
2022-07-16 00:26:00 -0500

    Add files via upload
    In the main py, there is the full code of the application. 
    The project will check the current location of the ISS and you with an ISS API, and DateTime lib. 
    When the ISS has similar lat, long location, smtp package will help to send you a reminder email.

---
+++ day33_api_kanyequote_and_more/day33_iss_is_above/day33_main.py
@@ -0,0 +1,56 @@
+import smtplib
+import requests
+from datetime import datetime
+import time
+
+
+MY_LAT = 41.878113 # Your latitude
+MY_LONG = -87.629799 # Your longitude
+
+MY_EMAIL = "testingemail@gmail.com"
+MY_PASSWORD = "1234"
+
+
+def is_iss_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+
+    #Your position is within +5 or -5 degrees of the ISS position.
+    if MY_LAT-5 <=iss_latitude<= MY_LAT+5 and MY_LONG-5 <= iss_longitude<= MY_LONG+5:
+        return True
+
+
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now().hour
+
+    if time_now >= sunset or time_now <= sunrise:
+        return True
+
+
+while True:
+    time.sleep(60)
+    if is_night() and is_iss_overhead():
+        connection = smtplib.SMTP("smtp.gmail.com")
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=MY_EMAIL,
+            msg="Subject: Look Up\n\n The ISS is above your head"
+        )
