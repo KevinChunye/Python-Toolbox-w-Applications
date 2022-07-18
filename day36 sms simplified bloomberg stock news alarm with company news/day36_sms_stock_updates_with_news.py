d33eac1d203edcceda6c9fd3691f45d856656fd1
Kevin Chunye <53375755+KevinChunye@users.noreply.github.com>
2022-07-18 17:23:46 -0500

    Add files via upload
    This program is a simplified version of stock alarm for Bloomberg and other stock platforms. 
    First, import certain packages and input your API key for the news API and the stock API.
    Second, pack those keys into a dictionary and feed them as para to request.get().
    Then, using list comprehension to loop and slice all the closing prices for yesterday.
    Fourth, slice the next element that represents 2 days ago closing price.
    After that, calculate the difference, diff_percent, and get a new var up_down and if statement to check whether the stock went up or down. 
    Then, write the main if statement, alarm if the stock has huge fluctuation, over 1% or something new, then use news API to get the top 3 news about that company today.
    Finally, editing the SMS message, telling the user which stock has XX% down/up, and the top 3 news about that company.

---
+++ day36 sms simplified bloomberg stock news alarm with company news/day36_sms_stock_updates_with_news.py
@@ -0,0 +1,78 @@
+import requests
+from twilio.rest import Client
+
+VIRTUAL_TWILIO_NUMBER = "VIRTUAL_TWILIO_NUMBER"
+VERIFIED_NUMBER = "Your personal phone number"
+
+STOCK_NAME = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+
+STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+
+# All the following code need to be changed based on your personal key and sid
+STOCK_API_KEY = "STOCK_API_KEY"
+NEWS_API_KEY = "NEWS_API_KEY"
+TWILIO_SID = 'TWILIO_SID'
+TWILIO_AUTH_TOKEN = 'TWILIO_AUTH_TOKEN'
+
+#Get yesterday's closing stock price
+stock_params = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK_NAME,
+    "apikey": STOCK_API_KEY,
+}
+
+response = requests.get(STOCK_ENDPOINT, params=stock_params)
+data = response.json()["Time Series (Daily)"]
+data_list = [value for (key, value) in data.items()]
+yesterday_data = data_list[0]
+yesterday_closing_price = yesterday_data["4. close"]
+print(yesterday_closing_price)
+
+#Get the day before yesterday's closing stock price
+day_before_yesterday_data = data_list[1]
+day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
+print(day_before_yesterday_closing_price)
+
+#Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20.
+difference = float(yesterday_closing_price) - float(day_before_yesterday_closing_price)
+up_down = None
+if difference > 0:
+    up_down = "🔺"
+else:
+    up_down = "🔻"
+
+#the percentage difference in price between closing price yesterday and closing price the day before yesterday.
+diff_percent = round((difference / float(yesterday_closing_price)) * 100,2)
+print(diff_percent)
+
+###you can change the diff_percent limit to see when do you want to receive the message
+#If difference percentage is greater than 0.1 then send message
+if abs(diff_percent) > 0.1:
+    news_params = {
+        "apiKey": NEWS_API_KEY,
+        "qInTitle": COMPANY_NAME,
+    }
+
+    news_response = requests.get(NEWS_ENDPOINT, params=news_params)
+    articles = news_response.json()["articles"]
+
+    #Use Python slice operator to create a list that contains the first 3 articles.
+    three_articles = articles[:3]
+    print(three_articles)
+
+    #Use Twilio to send a seperate message with each article's title and description to your phone number.
+
+    #Create a new list of the first 3 article's headline and description using list comprehension.
+    formatted_articles = [f"{STOCK_NAME}: {up_down}{diff_percent}%\nHeadline: {article['title']}. \nBrief: {article['description']}" for article in three_articles]
+    print(formatted_articles)
+    #Send each article as a separate message via Twilio.
+    client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+    for article in formatted_articles:
+        message = client.messages.create(
+            body=article,
+            from_=VIRTUAL_TWILIO_NUMBER,
+            to=VERIFIED_NUMBER
+        )
